bfa88b02e94d974b81326cd65f2f010f348e4fdb
manoday <manodayahire@gmail.com>
2026-02-01 11:00:11 +0530

    Update portfolio w

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -58,14 +58,7 @@ export default function Navbar() {
         transition={{ duration: 0.8 }}
       >
         <div className="container mx-auto flex items-center justify-between">
-          {/* Logo */}
-          <Link
-            href="/"
-            onClick={handleLogoClick}
-            className="text-xl font-bold tracking-tighter uppercase z-50 text-foreground dark:text-white"
-          >
-            Dev<span className="opacity-50">.Portfolio</span>
-          </Link>
+          
 
           {/* Desktop Nav */}
           <div className="hidden xl:flex items-center gap-8">
